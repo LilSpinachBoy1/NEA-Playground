404270899abca97c082f8fafc34f9f8a58315d7f
SamDoughtyCodes <samdcodes@gmail.com>
2025-06-20 11:08:44 +0100

    Got post request working

--- API/api.py
+++ API/api.py
@@ -1,5 +1,10 @@
 import sqlite3
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+# Create bade model for searching by ID
+class ID(BaseModel):
+    id: int
 
 # Set up db connection
 con = sqlite3.connect('fun_facts.db', check_same_thread=False)
@@ -25,3 +30,12 @@ def test():
     if run_query("SELECT * from facts;"): valid = True  # If the funtion returns something, the db is working
     else: valid = False
     return {"api": True, "db": valid}
+
+
+# Endpoint to get a fact from a sent ID
+@app.post("/api/search_id")
+def search(id: ID):
+    query = f"SELECT fact FROM facts WHERE id = {id.id};"
+    result = run_query(query)
+    if result: return {"fact": result[0][0]}
+    else: return {"fact": "No fact found with that ID OR an issue with the database."}
